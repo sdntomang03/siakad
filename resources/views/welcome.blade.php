93be7132114e63dd877e1d2cadbe5ec2c1c8d70b
sdntomang03 <sdntomang03jkt@gmail.com>
2026-06-18 20:22:12 +0700

    fixed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,11 +10,8 @@
     <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600,700,800" rel="stylesheet" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 
-    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    @else
-    <script src="https://cdn.tailwindcss.com"></script>
-    @endif
+
 
     <style>
         body {
