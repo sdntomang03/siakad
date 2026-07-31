6a683941a7e1bb3224d22f8e7bbd226d87a0f5e1
sdntomang03 <sdntomang03jkt@gmail.com>
2026-07-31 09:10:48 +0700

    fixed

--- routes/web.php
+++ routes/web.php
@@ -235,7 +235,7 @@ Route::middleware(['auth', 'role:guru'])->prefix('piket')->name('piket.')->group
     // Pengaturan Jadwal Master
     Route::get('/jadwal', [PiketController::class, 'jadwal'])->name('jadwal');
     Route::post('/jadwal', [PiketController::class, 'storeJadwal'])->name('jadwal.store');
-    Route::get('/piket/daftar-jadwal', [PiketController::class, 'daftarJadwal'])->name('piket.daftarJadwal');
+    Route::get('/daftar-jadwal', [PiketController::class, 'daftarJadwal'])->name('daftarJadwal');
 
     // Pencatatan Jurnal Harian
     Route::get('/jurnal', [PiketController::class, 'jurnal'])->name('jurnal');
