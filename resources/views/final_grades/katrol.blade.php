10d39382f7984b8d9543a8d5948770b722f5fb72
sdntomang03 <sdntomang03jkt@gmail.com>
2026-08-09 09:43:19 +0700

    fixed

--- resources/views/final_grades/katrol.blade.php
+++ resources/views/final_grades/katrol.blade.php
@@ -22,7 +22,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                             d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                     </svg>
-                    <span class="uppercase tracking-widest text-xs">Kesalahan Input:</span>
+                    <span class="text-sm">Terjadi kesalahan validasi. Silakan periksa kembali inputan Anda.</span>
                 </div>
                 <ul class="list-disc list-inside text-sm font-medium">
                     @foreach ($errors->all() as $error)
