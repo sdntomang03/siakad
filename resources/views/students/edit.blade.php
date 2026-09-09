1a0db872cc7e67a1b785495d6d845a815f90356f
sdntomang03 <sdntomang03jkt@gmail.com>
2026-09-09 14:41:52 +0700

    fixed

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -753,7 +753,8 @@
                                     $agamaAyahLama = old('agama_ayah', $student->student->family->agama_ayah ?? '');
                                     $agamaIbuLama = old('agama_ibu', $student->student->family->agama_ibu ?? '');
                                     $agamaWaliLama = old('agama_wali', $student->student->family->agama_wali ?? '');
-                                    $pendidikanOptions = ['Tidak tamat', 'SD', 'SMP', 'SMA', 'S1', 'S2', 'S3'];
+                                    $pendidikanOptions = ['Tidak tamat', 'SD/MI', 'SMP/MTs', 'SMA//SMK/MA', 'S1', 'S2',
+                                    'S3'];
                                     $pendidikanAyahLama = old('pendidikan_ayah',
                                     $student->student->family->pendidikan_ayah ?? '');
                                     $pendidikanIbuLama = old('pendidikan_ibu', $student->student->family->pendidikan_ibu
