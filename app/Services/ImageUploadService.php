c76762204c7dfcc733e8d220933c02de1d21846d
sdntomang03 <sdntomang03jkt@gmail.com>
2026-09-09 13:25:49 +0700

    fixed

--- app/Services/ImageUploadService.php
+++ app/Services/ImageUploadService.php
@@ -4,7 +4,7 @@ namespace App\Services;
 
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
+use Intervention\Image\Image;
 
 class ImageUploadService
 {
