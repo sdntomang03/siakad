a0ddc4e933c4f2d69ce340b9683f4d201380abab
sdntomang03 <sdntomang03jkt@gmail.com>
2026-09-09 11:02:13 +0700

    fixed

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -174,18 +174,16 @@
                                         <div>
                                             <label
                                                 class="block text-sm font-semibold text-slate-700 dark:text-slate-300 mb-1">NISN</label>
-                                            <input type="text" name="nisn"
+                                            <input readonly type="text" name="nisn"
                                                 value="{{ old('nisn', $student->student->nisn ?? '') }}"
-                                                class="block w-full rounded-lg border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-slate-900 dark:border-slate-600 dark:text-white transition-colors"
-                                                readonly>
+                                                class="block w-full rounded-lg border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-slate-900 dark:border-slate-600 dark:text-white transition-colors">
                                         </div>
                                         <div>
                                             <label
                                                 class="block text-sm font-semibold text-slate-700 dark:text-slate-300 mb-1">NIS</label>
-                                            <input type="text" name="nis"
+                                            <input readonly type="text" name="nis"
                                                 value="{{ old('nis', $student->student->nipd ?? '') }}"
-                                                class="block w-full rounded-lg border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-slate-900 dark:border-slate-600 dark:text-white transition-colors"
-                                                readonly>
+                                                class="block w-full rounded-lg border-slate-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm dark:bg-slate-900 dark:border-slate-600 dark:text-white transition-colors">
                                         </div>
                                         <div>
                                             <label
